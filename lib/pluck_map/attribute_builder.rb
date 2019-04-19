6416a880ecd23f7ef2de5bffeba51a7c215ae715
Bob Lail <bob.lailfamily@gmail.com>
2019-04-18 21:22:38 -0700

    [refactor] Inherit PluckMap::AttributeBuilder from BasicObject (5m)

--- lib/pluck_map/attribute_builder.rb
+++ lib/pluck_map/attribute_builder.rb
@@ -2,26 +2,27 @@ require "pluck_map/attribute"
 require "pluck_map/attributes"
 
 module PluckMap
-  class AttributeBuilder
+  class AttributeBuilder < BasicObject
 
     def self.build(&block)
-      builder = self.new
+      attributes = []
+      builder = self.new(attributes)
       if block.arity == 1
         block.call(builder)
       else
         builder.instance_eval(&block)
       end
-      Attributes.new(builder.instance_variable_get(:@attributes))
+      Attributes.new(attributes)
     end
 
-    def initialize
-      @attributes = []
+    def initialize(attributes)
+      @attributes = attributes
     end
 
     def method_missing(attribute_name, *args)
       options = args.extract_options!
       options[:value] = args.first unless args.empty?
-      @attributes.push PluckMap::Attribute.new(attribute_name, options)
+      @attributes.push Attribute.new(attribute_name, options)
       :attribute_added
     end
 
